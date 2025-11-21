d00af2d3e0fceed8d1c9d60fbb05a81b36c8bafb
karo <karolin.kostial@gmail.com>
2025-11-21 18:13:13 +0100

    Refactor `authorizeRepositoryAccess` for role-based access control
    - Implement role-based checks for repository access (`Admin`, `User`, `Guest` roles).
    - Add failure handling for unauthorized users and roles lacking sufficient permissions.
    - Enhance error handling with `WWW-Authenticate` header for unauthorized access.

--- internal/handlers/ocihandlers/utils.go
+++ internal/handlers/ocihandlers/utils.go
@@ -21,25 +21,51 @@ func checkAccess(
 	repository *repositories.Repository,
 	accessType string,
 ) error {
+	var repositoryAccessFilter *repositories.RepositoryAccessFilter
+	var repositoryAccess *repositories.RepositoryAccess
+	var err error
+
 	if repository.GetIsPublic() {
 		return nil
 	}
 
 	currentUser := authentication.GetCurrentUser(ctx)
 	if currentUser.UserId == uuid.Nil {
-		realm := fmt.Sprintf("%s/v2/token", config.C.Server.ExternalUrl)
-		service := fmt.Sprintf("%s:%s", config.C.Server.ExternalDomain, repoIdentifier.TenantSlug)
-		scope := fmt.Sprintf("repository:%s/%s/%s:%s", repoIdentifier.TenantSlug, repoIdentifier.ProjectSlug, repoIdentifier.RepositorySlug, accessType)
-
-		wwwAuthenticateHeaderValue := fmt.Sprintf("Bearer realm=\"%s\",service=\"%s\",scope=\"%s\"", realm, service, scope)
-
-		return ociError.NewOciError(ociError.Unauthorized).
-			WithMessage("user is not authenticated").
-			WithHttpCode(401).
-			WithHeader("WWW-Authenticate", wwwAuthenticateHeaderValue)
+		goto failure
 	}
 
+	repositoryAccessFilter = repositories.NewRepositoryAccessFilter().
+		ByRepositoryId(repository.GetId()).
+		ByUserId(currentUser.UserId)
+	repositoryAccess, err = tx.RepositoryAccess().First(ctx, repositoryAccessFilter)
+	if err != nil {
+		return fmt.Errorf("getting repository access: %w", err)
+	}
+	if repositoryAccess == nil {
+		goto failure
+	}
+
+	if accessType == "push" {
+		if repositoryAccess.GetRole() == repositories.RepositoryAccessRoleGuest {
+			goto failure
+		}
+	}
+
+	// pull access is allowed for all roles
+
 	return nil
+
+failure:
+	realm := fmt.Sprintf("%s/v2/token", config.C.Server.ExternalUrl)
+	service := fmt.Sprintf("%s:%s", config.C.Server.ExternalDomain, repoIdentifier.TenantSlug)
+	scope := fmt.Sprintf("repository:%s/%s/%s:%s", repoIdentifier.TenantSlug, repoIdentifier.ProjectSlug, repoIdentifier.RepositorySlug, accessType)
+
+	wwwAuthenticateHeaderValue := fmt.Sprintf("Bearer realm=\"%s\",service=\"%s\",scope=\"%s\"", realm, service, scope)
+
+	return ociError.NewOciError(ociError.Unauthorized).
+		WithMessage("user is not authenticated").
+		WithHttpCode(401).
+		WithHeader("WWW-Authenticate", wwwAuthenticateHeaderValue)
 }
 
 func getRepositoryByIdentifier(ctx context.Context, tx database.Transaction, repoIdentifier middlewares.OciRepositoryIdentifier) (*repositories.Tenant, *repositories.Project, *repositories.Repository, error) {
